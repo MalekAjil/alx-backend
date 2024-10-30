bde64b2a908e522b621c0aadbcc9d29c25d5d7df
MalekAjil <aboanasajil@gmail.com>
2024-10-30 15:51:44 -0700

    task5

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""LFU_cache"""
+"""LFUCache module"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache (BaseCaching):
-    """LFU Cache class"""
+    """LFU Cache system"""
     def __init__(self):
-        """iniitialization of class"""
+        """iniitialization"""
         super().__init__()
+        self.order = []
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item value for
@@ -15,14 +16,21 @@ class LFUCache (BaseCaching):
         If key or item is None, this method should not do anything.
         If the number of items in self.cache_data is higher that
         BaseCaching.MAX_ITEMS:
-        must discard the least frequently item put in cache (MRU algorithm)
-        must print DISCARD: with the key discarded and following by a new line
+        you must discard the least frequency used item (LFU algorithm)
+        if you find more than 1 item to discard,
+        you must use the LRU algorithm to discard only the least recently used
+        you must print DISCARD: with the key discarded and following by a new line
         """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                print("DISCARD:", self.cache_data.first().key)
-                self.cache_data.pop(first())
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lruk = self.order.pop(0)
+            del self.cache_data[lruk]
+            print("DISCARD:", lruk)
+        self.cache_data[key] = item
+        self.order.append(key)
 
     def get(self, key):
         """ return the value in self.cache_data linked to key.
@@ -30,4 +38,6 @@ class LFUCache (BaseCaching):
         return None."""
         if key is None or key not in self.cache_data:
             return None
+        self.order.remove(key)
+        self.order.append(key)
         return self.cache_data[key]
