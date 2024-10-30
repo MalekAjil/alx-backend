111ff073ad47ce60f685fbd66eb9e64a6a533d11
MalekAjil <aboanasajil@gmail.com>
2024-10-30 15:39:53 -0700

    task3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""LRU_cache"""
+"""lru_cache"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -8,6 +8,7 @@ class LRUCache (BaseCaching):
     def __init__(self):
         """iniitialization"""
         super().__init__()
+        self.order = []
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item value for
@@ -15,14 +16,19 @@ class LRUCache (BaseCaching):
         If key or item is None, this method should not do anything.
         If the number of items in self.cache_data is higher that
         BaseCaching.MAX_ITEMS:
-        must discard the least item put in cache (LRU algorithm)
+        must discard the least recently used item put in cache (FIFO algorithm)
         must print DISCARD: with the key discarded and following by a new line
         """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                print("DISCARD:", self.cache_data.first().key)
-                self.cache_data.pop(first())
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lruk = self.order.pop(0)
+            del self.cache_data[lruk]
+            print("DISCARD:", lruk)
+        self.cache_data[key] = item
+        self.order.append(key)
 
     def get(self, key):
         """ return the value in self.cache_data linked to key.
@@ -30,4 +36,6 @@ class LRUCache (BaseCaching):
         return None."""
         if key is None or key not in self.cache_data:
             return None
+        self.order.remove(key)
+        self.order.append(key)
         return self.cache_data[key]
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -8,3 +8,15 @@ What MRU means
 What LFU means
 What the purpose of a caching system
 What limits a caching system have
+
+# LRUCache
+This project implements a Least Recently Used (LRU) caching system in Python.
+The `LRUCache` class inherits from `BaseCaching` and follows the LRU algorithm to manage cache entries.
+## Requirements
+- Python 3.7 
+- Ubuntu 18.04 LTS 
+- Pycodestyle 2.5
+## Usage To use the `LRUCache` class,
+simply import it and create an instance.
+Use the `put` method to add items to the cache and the `get` method to retrieve items.
+## Example
