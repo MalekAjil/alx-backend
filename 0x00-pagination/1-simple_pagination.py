ef88eff1ff993aaa7b2aa765ef1eeba2d435e77a
MalekAjil <aboanasajil@gmail.com>
2024-11-01 20:05:37 -0700

    task2

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -2,14 +2,15 @@
 """simple_pagination module"""
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for
     those particular pagination parameters."""
     return ((page - 1) * page_size, page * page_size)
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
