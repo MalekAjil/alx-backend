3c0b47af1a8a0b467b7db9d9df413bf0f29ef90d
MalekAjil <aboanasajil@gmail.com>
2024-11-01 20:14:52 -0700

    task3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -14,6 +14,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """initialization constructor"""
         self.__dataset = None
         self.__indexed_dataset = None
 
